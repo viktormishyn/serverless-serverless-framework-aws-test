e333948d8ed7f2e68eda9e2a97aa1e109dff2628
viktormishyn <viktormishyn@gmail.com>
2021-06-22 22:28:54 +0300

    Refactor

--- first-serverless-aws-project/webpack.config.js
+++ first-serverless-aws-project/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    target: 'node',
-    mode: 'none', 
-    // 'none' means that we don't need the extra processing to minify the code (as it would be for 'production')
-}
+  target: "node",
+  mode: "none",
+  // 'none' means that we don't need the extra processing to minify the code (as it would be for 'production')
+};
